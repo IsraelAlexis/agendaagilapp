bb9d23bc5dd5e521ec7385af5735d2e5a8f1c5cd
Israel Alexis Alzate Tabares <iaalzate@sura.com.co>
2024-04-02 16:54:38 -0500

    actualizacion bases y react-routes-dom

--- src/Formulario/Formulario.jsx
+++ src/Formulario/Formulario.jsx
@@ -1,12 +1,23 @@
 import { useState } from "react"
-
+import { useNavigate } from "react-router-dom"
 import "./Formulario.css"
+
 export function Formulario(){
 
-    const[vercedula,modificarcedula]=useState("")
-    const[vercontraseña,modificarcontraseña]=useState("")
+    const[vercedula,guardarCedula]=useState("")
+    const[vercontraseña,guardarContraseña]=useState("")
 
-    function procesarFormulario(){
+    // para navegar entre componentes debo declarar una variable que almacene el hook
+    let enrutador=useNavigate()
+    
+
+    function procesarFormulario(evento){
+        // que hago si le hacen clic al boton del formulario
+        evento.preventDefault()
+        // console.log("le hicieron clic al boton de formulario")
+        
+        // voy a enrutar otro componente(¿como lanzo un componente desde otro?)
+        enrutador("/home")
         
     }
 
@@ -16,15 +27,18 @@ export function Formulario(){
             <div className="row justify-content-center text-center">
                 <div className="col-12 col-md-6">
                     <img src="../../src/assets/logo sura.webp" alt=""className="img-fluid" />
-                    <form className="border rounded p-4">
+                    <form className="border rounded p-4" onSubmit={procesarFormulario}>
                         <h2 className="fuente">Controla tu Salud</h2>
                         <div class="input-group mb-3 mt-5">
                         <span class="input-group-text" id="basic-addon1"><i class="bi bi-person-vcard"></i></span>
                         <input 
                             type="text" 
                             class="form-control" 
-                            placeholder="Numero Cedula" 
+                            placeholder="Número Cedula" 
                             id="identificacion"
+                            onChange={function(evento){
+                                guardarCedula(evento.target.value)
+                            }}
                         />
                         </div>
                         <div class="input-group mb-3 mt-5">
@@ -33,7 +47,10 @@ export function Formulario(){
                             type="password" 
                             class="form-control" 
                             placeholder="Contraseña"
-                            id="contraseña" 
+                            id="contraseña"
+                            onChange={function(evento){
+                                guardarContraseña(evento.target.value)
+                            }} 
                             />
                         </div>
                         <button type="submit" class="fondoboton">Iniciar sesión</button>
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import  'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
-import { Home } from './Home/Home'
 import 'bootstrap-icons/font/bootstrap-icons.css'
-import { Formulario } from './Formulario/Formulario'
+import {Rutas} from "./Rutas/Rutas"
+import { BrowserRouter } from 'react-router-dom'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Formulario></Formulario>
+    <BrowserRouter>
+    <Rutas></Rutas>
+    </BrowserRouter>
   </React.StrictMode>,
 )
